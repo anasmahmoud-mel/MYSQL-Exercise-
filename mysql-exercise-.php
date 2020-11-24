a2a78683763fb468b6b88892b28a80e5499a50f7
anasmahmoud-mel <71829391+anasmahmoud-mel@users.noreply.github.com>
2020-11-25 01:25:57 +0300

    Add files via upload

---
+++ mysql-exercise-.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.3
+ */
+
+/**
+ * Database `mysql-exercise-`
+ */
+
+/* `mysql-exercise-`.`countries` */
+$countries = array(
+);
+
+/* `mysql-exercise-`.`countries_q2` */
+$countries_q2 = array(
+);
+
+/* `mysql-exercise-`.`countries_q5` */
+$countries_q5 = array(
+);
+
+/* `mysql-exercise-`.`countries_q7` */
+$countries_q7 = array(
+);
+
+/* `mysql-exercise-`.`countries_q9` */
+$countries_q9 = array(
+);
+
+/* `mysql-exercise-`.`countries_q11` */
+$countries_q11 = array(
+);
+
+/* `mysql-exercise-`.`countries_q12` */
+$countries_q12 = array(
+);
+
+/* `mysql-exercise-`.`countries_q13` */
+$countries_q13 = array(
+);
+
+/* `mysql-exercise-`.`dup_countries` */
+$dup_countries = array(
+);
+
+/* `mysql-exercise-`.`dup_countries_q3` */
+$dup_countries_q3 = array(
+);
+
+/* `mysql-exercise-`.`employees_q15` */
+$employees_q15 = array(
+);
+
+/* `mysql-exercise-`.`employees_q16` */
+$employees_q16 = array(
+);
+
+/* `mysql-exercise-`.`employees_q17` */
+$employees_q17 = array(
+);
+
+/* `mysql-exercise-`.`employees_q18` */
+$employees_q18 = array(
+);
+
+/* `mysql-exercise-`.`employees_q19` */
+$employees_q19 = array(
+);
+
+/* `mysql-exercise-`.`employees_q20` */
+$employees_q20 = array(
+);
+
+/* `mysql-exercise-`.`jobs` */
+$jobs = array(
+);
+
+/* `mysql-exercise-`.`jobs_q6` */
+$jobs_q6 = array(
+);
+
+/* `mysql-exercise-`.`job_history` */
+$job_history = array(
+);
+
+/* `mysql-exercise-`.`job_historyq14` */
+$job_historyq14 = array(
+);
